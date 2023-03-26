9f5b32aae009695fa81ff14cb9b3cd72cf89da41
Gorbunov Vladislav <wint-mil@mail.ru>
2023-03-26 20:08:04 +0300

    init commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+'use strict';
+a=5;
+console.log(a);
+var leftBorderWidth = 1;
+
+{
+    let second = 2;
+}
+const pi = 3.14;
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log("sds"/0);
+
+let something;
+console.log(something);
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['p.png', '12.jpg', 'sum.bmp'];
+
+console.log(arr[0]);
+
+// alert("hello world")
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = +prompt("Есть вам 18?", "Да");
+// console.log(answer);
+// console.log(typeof(answer));
+
+// console.log(4 + +" - ob");
+
+console.log("2" === "3");
+
+console.log(true && true);
